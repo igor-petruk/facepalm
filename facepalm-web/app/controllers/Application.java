efdaf0d5551e5645477663a2f00cf67ac5223cce
Volodymyr Krasnikov <VKrasnikov@gmail.com>
2012-11-10 14:31:51 +0200

    count method returns JSON

--- facepalm-web/app/controllers/Application.java
+++ facepalm-web/app/controllers/Application.java
@@ -5,12 +5,24 @@ import play.mvc.*;
 
 import java.util.*;
 
-import models.*;
-
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index()
+	{
+		Logger.info(">>> Index page <<<");
+		render();
+	}
+
+	public static void count(String siteUrl, String imageUrl)
+	{
+		Logger.info(">>> count <<< site url : %s image url : %s", siteUrl, imageUrl);
+		String countResult = String.format("{\"count\": %d}", 0);
+		renderJSON(countResult);
+	}
+
+	public static void like(String siteUrl, String imageUrl, String userLogonId)
+	{
+
+	}
 
 }
