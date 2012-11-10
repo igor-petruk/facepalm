6aa2f1e9d417097d08ffd419c0fe9a4c9a600fa1
Igor Petruk <igor.petrouk@gmail.com>
2012-11-10 23:39:39 +0200

    Improved design of login and user script

--- facepalm-web/public/javascripts/facepalm.userscript.js
+++ facepalm-web/public/javascripts/facepalm.userscript.js
@@ -7,6 +7,7 @@
 // @require http://localhost:9000/public/javascripts/jquery-1.8.2.js
 // @require http://localhost:9000/public/javascripts/jquery.qtip.js
 // @resource qtipCSS http://localhost:9000/public/stylesheets/jquery.qtip.css
+// @resource bootstrapCSS http://localhost:9000/public/bootstrap/css/bootstrap.min.css
 // @include http://*
 // ==/UserScript==
 (function (window, undefined) {
@@ -19,7 +20,6 @@
     if (w.self != w.top) {
         return;
     }
-
     function runCodeOnPage(code){
         var script = document.createElement('script');
         script.textContent = code;
@@ -27,23 +27,20 @@
         script.parentNode.removeChild(script);
     }
 
-
-    function likeImage(imageUrl, counterSpan, mainSpan){
-        var success = false;
+    function likeImage(imageUrl, link, liked, counterSpan, mainSpan){
         $.ajax({
             url: 'http://localhost:9000/like/'+encodeURIComponent(window.location.toString())+"/"+encodeURIComponent(imageUrl),
             type: 'POST',
-            async:false,
             dataType: "json",
             success: function(data, status) {
                 $(counterSpan).text(data.count);
-                success = true;
+                setLikeLinkName(link,liked);
             },
             error: function(jqXHR, textStatus, errorThrown){
                 if (jqXHR.status==403){
                     $(mainSpan).html($("<a>").text("Login").attr("href","#").on("click",function(event){
                         var url = 'http://localhost:9000/miniLogin/'+encodeURIComponent(window.location.toString())+"/"+encodeURIComponent(imageUrl);
-                        w.open(url,'loginWindow','height=200,width=300');
+                        w.open(url,'loginWindow','height=300,width=400,left=200,top=200,scrollbars=0');
                         event.preventDefault ? event.preventDefault() : event.returnValue = false;
                     }));
                 }
@@ -57,10 +54,14 @@
 
     $('img').each(function() {
         var imgSrc = $(this).attr("src");
+
+        var loader = $("<img>").attr("src","http://localhost:9000/public/images/ajax-loader.gif");
+        var initialLoader = $("<span>").append(loader).append("          ");
+
         $(this).qtip({
             // content: '<a href="#">Edit</a> | <a href="#">Delete</a>', // Give it some content
             content:{
-                text: 'Loading...', // The text to use whilst the AJAX request is loading
+                text: initialLoader, // The text to use whilst the AJAX request is loading
                 ajax: {
                     url: 'http://localhost:9000/count/'+encodeURIComponent(window.location.toString())+"/"+encodeURIComponent(imgSrc), // URL to the local file
                     once: false,
@@ -77,9 +78,9 @@
                         a.on("click",function(e){
                             //if (data.loggedIn=="true"){
                             if (true){
+                                counter.html(loader);
                                 liked = !liked;
-                                setLikeLinkName(a,liked);
-                                likeImage(imgSrc, counter, span);
+                                likeImage(imgSrc, a,liked,counter, span);
                             }else{
                             }
                         });
@@ -95,16 +96,19 @@
                 fixed: true // Make it fixed so it can be hovered over
             },
             style: {
-                padding: '5px 15px', // Give it some extra padding
-                name: 'dark' // And style it with the preset dark theme
+                tip: {
+                    corner: false
+                },
+                classes:"ui-tooltip-tipped",
+                width:70,
+                padding: '2px 7px'// Give it some extra padding
             }
         });
     });
-    GM_addStyle(".facepalm{text-decoration:  none}");
-    GM_addStyle(".facepalm-shown{.facepalm; display:block;}");
-    GM_addStyle(".facepalm-hidden{.facepalm; display:none;}");
     var qtipCSS = GM_getResourceText ("qtipCSS");
     GM_addStyle (qtipCSS);
+    // GM_addStyle ( GM_getResourceText ("bootstrapCSS"));
+
     // load jQuery and execute the main function
     //addJQuery(main);
 })(window);
