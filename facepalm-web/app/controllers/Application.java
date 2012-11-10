121d8be79dd056f137b0e78d4c0e7100ceec5899
VlodymyrD <hagen.pereplut@gmail.com>
2012-11-10 14:34:22 +0200

    added some fb logic

--- facepalm-web/app/controllers/Application.java
+++ facepalm-web/app/controllers/Application.java
@@ -1,14 +1,45 @@
 package controllers;
 
 import play.*;
+import play.modules.facebook.FbGraph;
+import play.modules.facebook.FbGraphException;
 import play.mvc.*;
+import play.mvc.Scope.Session;
 
 import java.util.*;
 
+import com.google.gson.JsonObject;
+import com.restfb.FacebookClient;
+import com.restfb.types.User;
+
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		render();
+	}
+	
+	public static void index(String email) {
+		render(email);
+	}
+
+	public static void facebookLogin() {
+		
+		FacebookClient fbClient = FbGraph.getFacebookClient();
+		User profile = fbClient.fetchObject("me", com.restfb.types.User.class);
+		String email = profile.getEmail();
+		// do useful things
+		Session.current().put("username", email); 
+
+		index(email);
+	}
+
+	public static void facebookLogout() {
+		Session.current().remove("username");
+		FbGraph.destroySession();
+		index();
+	}
+	public static void users(){
+		render();
+	}
 
 }
