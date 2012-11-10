e83bed144c7ec4ef826cf78a5c908a4fd2ac4329
VlodymyrD <hagen.pereplut@gmail.com>
2012-11-10 17:24:34 +0200

    login to facebook logic added

--- facepalm-web/app/controllers/Application.java
+++ facepalm-web/app/controllers/Application.java
@@ -10,27 +10,44 @@ import java.util.*;
 
 import com.google.gson.JsonObject;
 import com.restfb.FacebookClient;
+import com.restfb.exception.FacebookOAuthException;
 import com.restfb.types.User;
 
 public class Application extends Controller {
 
 	public static void index() {
-		render();
+		try{
+			FacebookClient fbClient = FbGraph.getFacebookClient();
+			User profile = fbClient.fetchObject("me", com.restfb.types.User.class);
+			Logger.info("profile=%s", profile.getName());
+			user(profile.getName(),profile.getId());
+		}catch(Exception ex){
+			//not logged in, show button	
+			login();
+		}	
 	}
 	
-	public static void index(String email) {
-		render(email);
+	public static void user(String name,String id){
+		render(name,id);
 	}
+	
+	public static void login(){
+		boolean isShowLoginButton=true;
+		render(isShowLoginButton);
+	}	
+	
 
 	public static void facebookLogin() {
 		
 		FacebookClient fbClient = FbGraph.getFacebookClient();
+		
 		User profile = fbClient.fetchObject("me", com.restfb.types.User.class);
-		String email = profile.getEmail();
-		// do useful things
-		Session.current().put("username", email); 
+		String uid = profile.getId()+" ";
+		String name = profile.getFirstName();				
+		 
+		Session.current().put("username", uid); 
 
-		index(email);
+		index();
 	}
 
 	public static void facebookLogout() {
