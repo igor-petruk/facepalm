100549546da25eacd358c4e72336d0ef874ec895
Volodymyr Krasnikov <VKrasnikov@gmail.com>
2012-11-10 20:26:13 +0200

    migrated to H2 and JPA

--- facepalm-web/app/controllers/Application.java
+++ facepalm-web/app/controllers/Application.java
@@ -1,6 +1,10 @@
 package controllers;
 
+import javax.persistence.TypedQuery;
+
+import models.ImageEntity;
 import play.Logger;
+import play.db.jpa.JPA;
 import play.modules.facebook.FbGraph;
 import play.mvc.Controller;
 import play.mvc.Http;
@@ -17,6 +21,8 @@ import domain.SocialApplication;
 
 public class Application extends Controller {
 
+	static SocialApplication APP = SocialApplication.FACEBOOK;
+	
 	public static void index() {
 		render();
 	}
@@ -25,32 +31,78 @@ public class Application extends Controller {
 		render(email);
 	}
 	
+//	public static void count(String siteUrl, String imageUrl)
+//	{
+//		Integer value = LikeRepository.getLikeCount(siteUrl, imageUrl);
+//		String result = JsonResponse.getCount(value);
+//		
+//		renderJSON(result);
+//	}
+	
 	public static void count(String siteUrl, String imageUrl)
 	{
-		Logger.info("Site url : %s image url : %s", siteUrl, imageUrl);
 		
-		boolean isLoogedIn = true;
-		Integer value = LikeRepository.getLikeCount(siteUrl, imageUrl);
-		String result = JsonResponse.getCount(value, isLoogedIn);
+		boolean wasLiked = false;
+		if( LoginManager.isLoggedIn(APP, Session.current() ) ){
+			String uToken = LoginManager.loggedUserToken(APP, Session.current() );
+			ImageEntity ie = ImageEntity.find("siteUrl = ? and imageUrl = ? and userToken = ?", siteUrl, imageUrl, uToken).first();
+			wasLiked = ie != null;
+		}
+		
+		long value = ImageEntity.count("siteUrl = ? and imageUrl = ?", siteUrl, imageUrl);
+		
+		String result = JsonResponse.getCount( value, wasLiked );
 		
 		renderJSON(result);
 	}
 
+//	public static void like(String siteUrl, String imageUrl)
+//	{
+//		Logger.info("Site url : %s image url : %s", siteUrl, imageUrl);
+//		
+//		Session s = Session.current();
+//		SocialApplication app = SocialApplication.FACEBOOK;
+//		
+//		if ( LoginManager.isLoggedIn(app, s) ){
+//			
+//			Integer likeCount = LikeRepository.like(siteUrl, imageUrl);
+//			
+//			String countResult = JsonResponse.getCount(likeCount);
+//			
+//			renderJSON(countResult);
+//			
+//		} else {
+//			Response.current().status = Http.StatusCode.FORBIDDEN;
+//		}
+//
+//	}
+	
 	public static void like(String siteUrl, String imageUrl)
 	{
-		Logger.info("Site url : %s image url : %s", siteUrl, imageUrl);
 		
 		Session s = Session.current();
-		SocialApplication app = SocialApplication.FACEBOOK;
 		
-		if ( LoginManager.isLoggedIn(app, s) ){
+		if ( LoginManager.isLoggedIn(APP, s) ){
 			
-			Integer likeCount = LikeRepository.like(siteUrl, imageUrl);
+			ImageEntity ie = new ImageEntity();
 			
-			String countResult = JsonResponse.getCount(likeCount, true);
+			ie.setSiteUrl(siteUrl);
+			ie.setImageUrl(imageUrl);
+			ie.setUserToken(LoginManager.loggedUserToken(APP, s));
 			
-			renderJSON(countResult);
-			
+			validation.valid(ie);
+			if( validation.hasErrors() ){
+				
+				ie.save();
+				
+				long value = ImageEntity.count("siteUrl = ? and imageUrl = ?", siteUrl, imageUrl);
+				
+				String countResult = JsonResponse.getCount( value, true);
+				
+				renderJSON(countResult);
+			} else {
+				Response.current().status = Http.StatusCode.INTERNAL_ERROR;
+			}
 		} else {
 			Response.current().status = Http.StatusCode.FORBIDDEN;
 		}
