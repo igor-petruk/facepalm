d284a570b389e57a9e4d6371ca8373d3aac0f264
VlodymyrD <hagen.pereplut@gmail.com>
2012-11-11 09:31:46 +0200

    login fixed

--- facepalm-web/app/controllers/Application.java
+++ facepalm-web/app/controllers/Application.java
@@ -1,5 +1,11 @@
 package controllers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
--- facepalm-web/app/models/ImageEntity.java
+++ facepalm-web/app/models/ImageEntity.java
@@ -10,11 +10,11 @@ import play.db.jpa.Model;
 
 @Entity
 public class ImageEntity extends Model implements Comparable<ImageEntity> {
-	
-	@Column
+
+    @Column(length = 4000)
 	String siteUrl;
-	
-	@Column
+
+    @Column(length = 4000)
 	String imageUrl;
 	
 	@Column
@@ -134,7 +134,7 @@ public class ImageEntity extends Model implements Comparable<ImageEntity> {
 	@Override
 	public int compareTo(ImageEntity o)
 	{
-		return date.compareTo(o.date);
+		return o.date.compareTo(date);
 	}
 	
 }
